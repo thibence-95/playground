23eee6e085e0d3de445e7da15bb3397884a1a415
moeti <moetichentuf@hotmail.com>
2020-05-26 09:59:12 +0200

    Updates

---
+++ precache-manifest.08c108f819e34d18da57567a99271cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6ee97d5493718ba8dc5388cdd7a05a",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "a409f04d84ab60d0b6e8",
+    "url": "/news/static/css/main.468ada7f.chunk.css"
+  },
+  {
+    "revision": "b87393157f94c2eaf81e",
+    "url": "/news/static/js/2.554c57a5.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/news/static/js/2.554c57a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a409f04d84ab60d0b6e8",
+    "url": "/news/static/js/main.f5ffe5de.chunk.js"
+  },
+  {
+    "revision": "39b58e93bd57f6bbb9e9",
+    "url": "/news/static/js/runtime-main.76c0a1c6.js"
+  }
+]);
